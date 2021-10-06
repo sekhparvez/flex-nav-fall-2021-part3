f982b97e8317b055d41aa95f72f1412a81d35e4e
sekhparvez <sekhparvez@gmail.com>
2021-10-05 20:53:10 -0400

    done

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,22 +1,49 @@
+//variables
+
 var tabs = document.querySelectorAll('nav a')
 var contentPara = document.querySelector('.content')
 
-document.querySelector('nav a').classList.add('active')
 
-contentPara.innerHTML = data.cuisines
 
-document.addEventListener('click', makeActive)
 
-function makeActive(event) {
-  if (!event.target.matches('a')) return
-
-  makeInactive()
-  event.target.classList.add('active')
-
-  var type = window.location.hash.substring(1)
-  contentPara.innerHTML = data[type]
+// when the hash changes
+function setActiveTabAccordingToHash(type) {
+  makeAllTabsInactive();
+  var tabToActivate = document.querySelector(`a[href="#${type}"]`);
+  tabToActivate.classList.add("active");
 }
 
-function makeInactive() {
+
+function makeAllTabsInactive() {
   tabs.forEach((tab) => tab.classList.remove('active'))
 }
+//runs on page loads and when has changes
+function setContentAccordingToHash() {
+  const type = window.location.hash.substring(1);
+  data.forEach(function (item) {
+
+    if (item.section === type) {
+      contentPara.innerHTML = `<h2>${item.section}</h2> <p>${item.story}</p>`
+      setActiveTabAccordingToHash(type);
+    }
+  });
+}
+
+//initalize page when page loads
+function initializePage(){
+  if(!window.location.hash){
+    window.location.hash = "cuisines";
+    document.querySelector("[href]=#cuisines").classList.add("active");
+  }
+  else{
+    document.querySelector(`[href= "${window.location.hash}]"`);
+  }
+  
+  setContentAccordingToHash();
+}
+//event listeners
+window.addEventListener("hashchange", setContentAccordingToHash)
+
+
+//initializePage();
+document.addEventListener("DOMContentLoaded", initializePage);
